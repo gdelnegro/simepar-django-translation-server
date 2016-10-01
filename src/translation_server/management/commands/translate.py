b523449c01eff16647e2e0f17c5a5611cfc85a9b
Gustavo Del Negro <gustavodelnegro@gmail.com>
2016-10-01 20:28:13 -0300

    Updated translation Added migration for translationType translations
    Signed-off-by: Gustavo Del Negro <gustavodelnegro@gmail.com>

--- src/translation_server/management/commands/translate.py
+++ src/translation_server/management/commands/translate.py
@@ -7,7 +7,9 @@ from django.core.management import call_command
 class Command(BaseCommand):
     help = "This command executes all steps for translation"
 
+    app_name = "translation_server"
+
     def handle(self, *args, **options):
         call_command('make_translation_migrations')
-        call_command('migrate', 'portfolio')
+        call_command('migrate', self.app_name)
         call_command('make_translation')
