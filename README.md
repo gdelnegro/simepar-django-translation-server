d4534613801f594c8a8bd7d384bdd9c1a45de0ef
Gustavo Del Negro <gustavodelnegro@gmail.com>
2016-10-02 16:11:17 -0300

    Added todo list to readme.md; added validation to form
    Signed-off-by: Gustavo Del Negro <gustavodelnegro@gmail.com>

--- README.md
+++ README.md
@@ -4,3 +4,10 @@ Translation project for django
 [![Coverage Status](https://coveralls.io/repos/github/gdelnegro/simepar-django-translation-server/badge.svg?branch=master)](https://coveralls.io/github/gdelnegro/simepar-django-translation-server?branch=master)
 
 [![Build Status](https://travis-ci.org/gdelnegro/simepar-django-translation-server.svg?branch=master)](https://travis-ci.org/gdelnegro/simepar-django-translation-server)
+
+#todo:
+
+
+```
+[ ] 001: Add suport to N languages, right now it suport only EN and PT-BR
+```
--- src/translation_server/forms.py
+++ src/translation_server/forms.py
@@ -6,6 +6,20 @@ from translation_server.models import *
 
 
 class TranslationAdminForm(forms.ModelForm):
+    def clean(self):
+        cleaned_data = super(TranslationAdminForm, self).clean()
+        auxiliary_text_en = cleaned_data.get("auxiliary_text_en")
+        primary_text_en = cleaned_data.get("text_en")
+        auxiliary_text_pt_br = cleaned_data.get("auxiliary_text_pt_br")
+        primary_text_pt_br = cleaned_data.get("text_pt_br")
+
+        if auxiliary_text_en == primary_text_en:
+            self.add_error('auxiliary_text_en', forms.ValidationError(_('ME1'), code='identical_texts_en'))
+        if auxiliary_text_pt_br == primary_text_pt_br:
+            self.add_error('auxiliary_text_pt_br', forms.ValidationError(_('ME1'), code='identical_texts_pt_br'))
+
+        return cleaned_data
+
     def save(self, commit=False):
         translation = super(TranslationAdminForm, self).save(commit=commit)
         translation.save()
